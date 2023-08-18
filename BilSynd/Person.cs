56dacbf66d38ab2e9c8adabeba1d905997e8f87d
HenrikVP <hpo@tec.dk>
2023-08-18 09:57:58 +0200

    Ternary and stuff

--- BilSynd/Person.cs
+++ BilSynd/Person.cs
@@ -2,9 +2,6 @@
 {
     internal class Person
     {
-        public static string FirstStatic;
-
-        //Navn Efternavn Telefonnr
         public string Firstname { get; set; }
         public string Lastname { get; set; }
         public string PhoneNumber { get; set; }
--- BilSynd/Program.cs
+++ BilSynd/Program.cs
@@ -1,79 +1,130 @@
-﻿namespace BilSynd
+﻿using System.Globalization;
+
+namespace BilSynd
 {
+    //TODO Tell customer that a full list of cars every time menu is shown is overkill.
+
     internal class Program
     {
+        static Random random = new Random();
         //static Person[] people = new Person[2];
-        static List<Person> peopleList = new List<Person>();
+        //static List<Person> peopleList = new List<Person>();
+        static List<Car> carList = new List<Car>();
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-            new Person();
-            Person p1 = new Person();
-            p1.Firstname = "Grethe";
-            p1.PhoneNumber = "1234567890";
+            while (true) { Menu(); }
+        }
 
-            Person p2 = new Person();
-            p2.Firstname = "Ursula";
-            p2.PhoneNumber = "1234567890";
-            Person.FirstStatic = "Something different";
+        #region Menu
+        static void Menu()
+        {
+            Console.WriteLine("\n*** Menu ***\n");
+            Console.WriteLine("Select 1 for Show all cars");
+            Console.WriteLine("Select 2 for Create car");
+            Console.WriteLine("Select 3 for Search car");
+            Console.WriteLine("Select 4 for Gen LP");
 
-            //people[0] = p1;
-            //people[1] = p2;
-
-            peopleList.Add(p1);
-            peopleList.Add(p2);
-
-            peopleList.Remove(p2);
-            while (true)
+            switch (Console.ReadKey(true).Key)
             {
-                Person person = CreatePerson();
-                AddToArray(person);
-                ShowPeople();
+                case ConsoleKey.D1:
+                    ShowCars();
+                    break;
+                case ConsoleKey.D2:
+                    SomethingCar();
+                    break;
+                case ConsoleKey.D4:
+                    for (int i = 0; i < 100; i++) Console.WriteLine(GenerateRandomLicensePlate());
+                    break;
+                default:
+                    Console.WriteLine("Not understood");
+                    break;
             }
         }
 
-        static void ShowPeople()
+        private static void SomethingCar()
         {
-            foreach (Person person in peopleList)
+            Car car = CreateCar();
+            car.Owner = CreatePerson();
+            carList.Add(car);
+            //Variable   Condition           True                 False
+            string str = NeedInspection(car) ? "Bilen skal synes" : "Bilen skal IKKE synes";
+            Console.WriteLine(str);
+        }
+        #endregion
+
+        static string GenerateRandomLicensePlate()
+        {
+            char a1 = (char)(random.Next(25) + 65);
+            char a2 = (char)(random.Next(25) + 65);
+            string b = random.Next(100).ToString("00");
+            string c = random.Next(1000).ToString("000");
+            return a1.ToString() + a2.ToString() + " " + b + " " + c;
+        }
+
+        //TODO Search function
+
+        static void ShowCars()
+        {
+            Console.WriteLine("*** List of cars ***");
+            foreach (Car car in carList)
             {
-                if (person == null) continue;
-                Console.WriteLine("Navn: " + person.Firstname);
+                ShowCar(car);
             }
         }
 
-        static void AddToArray(Person person)
+        static bool NeedInspection(Car car)
         {
-            for (int i = 0; i < people.Length; i++)
-            {
-                if (people[i] == null)
-                {
-                    people[i] = person;
-                    return;
-                }
-            }
-            ExpandArray();
-            people[people.Length - 1] = person;
+            //If car year + 5 is more than now year, then no inspection
+            if (car.DateofRegistration.AddYears(5) >= DateTime.Now) return false;
+            //If last inspeciton + 2 is more than now year, then no inspection
+            if (car.LastInspection.AddYears(2) >= DateTime.Now) return false;
+            return true;
         }
 
-        static void ExpandArray()
+        static void ShowCar(Car car)
         {
-            Person[] temp = new Person[people.Length + 1];
-            people.CopyTo(temp, 0);
-            people = temp;
+            Console.WriteLine($"\nCar: {car.Brand} {car.Model} \tLicense plate: {car.LicensePlate}");
+            Console.WriteLine($"Reg.Date: {car.DateofRegistration.ToString(CultureInfo.CurrentCulture.DateTimeFormat.ShortDatePattern)} \tLast inspection {car.LastInspection.ToString(CultureInfo.CurrentCulture.DateTimeFormat.ShortDatePattern)}");
+            Console.WriteLine($"Owner: {car.Owner.Firstname} {car.Owner.Lastname} \tPhone number: {car.Owner.PhoneNumber}");
         }
 
-        static void AddToList(Person person)
+        static Car CreateCar()
         {
-            peopleList.Add(person);
+            Console.WriteLine("\n***Create Car Menu***\n");
+
+            Car car = new Car();
+            car.DateofRegistration = RandomDay(1990);
+            car.ModelYear = car.DateofRegistration.Year - random.Next(2);
+            car.LastInspection = RandomDay(car.ModelYear);
+            car.EngineSize = 1 + random.Next(10) / 10f;
+            car.LicensePlate = GenerateRandomLicensePlate();
+
+            Console.Write("Brand: ");
+            car.Brand = Console.ReadLine();
+            Console.Write("Model: ");
+            car.Model = Console.ReadLine();
+
+            return car;
+        }
+
+        static DateTime RandomDay(int year)
+        {
+            DateTime start = new DateTime(year, 1, 1);
+            int range = (DateTime.Today - start).Days;
+            return start.AddDays(random.Next(range));
         }
 
         static Person CreatePerson()
         {
+            Console.WriteLine("\n*** Create Customer Menu ***\n");
+
             Person person = new Person();
-            Console.WriteLine("What is thy name?");
+            Console.Write("First name: ");
             person.Firstname = Console.ReadLine();
-            Console.WriteLine("What is thy number?");
+            Console.Write("Last name: ");
+            person.Lastname = Console.ReadLine();
+            Console.Write("Telephone number: ");
             person.PhoneNumber = Console.ReadLine();
             return person;
         }
